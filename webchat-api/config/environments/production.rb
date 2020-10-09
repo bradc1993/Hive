958f8e583f03ef67bcd8b8ef26d5cfa9c8d4ae27
bradc1993 <bradc1993@gmail.com>
2020-10-09 02:09:43 -0500

    updated action cable's allowed request origins

--- webchat-api/config/environments/production.rb
+++ webchat-api/config/environments/production.rb
@@ -102,4 +102,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_cable.allowed_request_origins = [
+    'http://localhost:3001',
+    'https://musing-spence-dec69b.netlify.app'
+  ]
+
 end
